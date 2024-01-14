cd77ed7d735bb5b9cd74409bf0cc8885ca2176fd
momo <mhamedbattioui@gmail.com>
2024-01-14 13:08:05 -0500

    update

---
+++ tetration/AdvancedCpp.cpp
@@ -0,0 +1,32 @@
+#include <iostream>             //input output stream library
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+//      int a = 100, b = 200;
+//      std::cout << b;                         // 0b10110101 0xf11d42a
+//      double d = a+b*a-b/3 % 1;               // auto replace var
+        /*
+        std::cout << "Hello Morld!" << std::endl << "Hello Zorld!\n";
+        cout << "entrez l'operation" << endl;
+        string entree = "";
+        cin >> entree;
+        cout << entree << endl;
+        */
+        
+	long long x = 5L;
+	cout << x << endl;
+	x = pow(5L,x);
+	cout << x << endl;
+	x = pow(5L,x);
+	cout << x << endl;
+	x = pow(5L,x);
+	cout << x << endl;
+	x = pow(5L,x);
+
+	
+	return 0;
+}
+
---
+++ tetration/AdvancedPython.py
@@ -0,0 +1,64 @@
+x = 5
+
+expo = 3125
+
+multiAPart = 1
+
+def chaineExpo(x, expo, multiAPart):
+    if expo == 0:
+        x = 1
+    elif expo == 1:
+        x = x
+    elif expo % 2 == 0:
+        expo = expo / 2
+        x = x**2
+    elif expo % 2 == 1:
+        expo = (expo-1) /2
+        multiAPart = multiAPart * x
+        x = x**2
+
+
+
+while expo > 1:
+    chaineExpo(x, expo, multiAPart)
+    print(x, "\n E = ", expo, "\n M = ", multiAPart, "\n x = ")
+
+#x = x ** expo
+#expo = x
+#x = 5
+
+
+
+
+
+
+#x**0 = 1
+#x ** 1 = x
+
+#x ** (2i) = (x**2) ** i
+#x ** (2i) = x * (x**2) ** i
+
+#karatsuba
+#xxxx * yyyy = xx*yy truc xx*yy
+
+
+
+
+
+
+
+
+#x = 5
+#print(x)
+#print("\n\n")
+#x = 5**x
+#print(x)
+#print("\n\n")
+#x = 5**x
+#print(x)
+#print("\n\n")
+#x = 5**x
+#print(x)
+#print("\n\n")
+#x = 5**x
+#print(x)
